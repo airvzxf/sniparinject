ee8bde66d94cf93a7142064d693d5c69e4b53d45
Israel Alberto RV <israel.alberto.rv@gmail.com>
2021-06-17 02:57:54 -0500

    Fixed the setup's scripts.

--- script/setup-dev.bash
+++ script/setup-dev.bash
@@ -4,11 +4,13 @@ set -ev
 CURRENT_PATH=$(dirname "${0}")
 cd "${CURRENT_PATH}" || exit
 
-cd ../
+cd ../src || exit
 rm -fR venv
-python3 -m venv venv
-source ./venv/bin/activate
+python3 -m venv ../venv
+source ../venv/bin/activate
 pip install --upgrade setuptools pip
-pip install -r requirements.txt
+pip install -r ../requirements.txt
+cp -p ../README.md ../LICENSE .
 pip install -e .
+rm -f README.md LICENSE
 deactivate
