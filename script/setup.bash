0a3c2ff9ce9e73cfb5c78b21dea89080ebcbfda6
Israel Alberto RV <israel.alberto.rv@gmail.com>
2021-06-17 02:44:25 -0500

    Fixed the setup's scripts.

--- script/setup.bash
+++ script/setup.bash
@@ -4,10 +4,10 @@ set -ev
 CURRENT_PATH=$(dirname "${0}")
 cd "${CURRENT_PATH}" || exit
 
-source ../venv/bin/activate
 cd ../
 rm -fR venv
 python3 -m venv venv
+source ./venv/bin/activate
 pip install --upgrade setuptools pip
 pip install -r ../requirements.txt
 deactivate
